38022ea5f4929df3a6b43004bab4dc82e6396f6f
Javier Combarros <Javier@138.4.192.251>
2014-10-20 00:01:27 +0200

    LinkImplementacion added

---
+++ src/main/java/es/jab/ConnectionState/LinkImplementacion.java
@@ -0,0 +1,27 @@
+package es.jab.ConnectionState;
+
+public class LinkImplementacion implements Link {
+
+	private String msg;
+
+    private int respuesta;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public int getRespuesta() {
+        return respuesta;
+    }
+
+    @Override
+    public void enviar(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void recibir(int respuesta) {
+        this.respuesta = respuesta;
+    }
+
+}
